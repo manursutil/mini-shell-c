9dd868c7e64c99ae900805714ad54541028a5710
Manuel Rodríguez Sutil <mrodsut@gmail.com>
2026-01-19 17:39:23 +0000

    fixed formatting

--- sh.c
+++ sh.c
@@ -16,7 +16,8 @@ void trim(char* line)
 int get_args_length(char** args)
 {
 	int count = 0;
-	while (args[count] != NULL && count < TOK_BUFF_SIZE) count++;
+	while (args[count] != NULL && count < TOK_BUFF_SIZE)
+		count++;
 	return count;
 }
 
@@ -25,7 +26,8 @@ int check_pipes(char** args)
 	int length = get_args_length(args);
 	for (int i = 0; i < length; i++)
 	{
-		if (strcmp(args[i], "|") == 0) return 1;
+		if (strcmp(args[i], "|") == 0)
+			return 1;
 	}
 	return 0;
 }
@@ -36,16 +38,16 @@ char*** split_pipes(char** args) // input: {"ls", "-lh", "|", "cd", NULL}
 	int len = get_args_length(args);
 	for (int i = 0; i < len; i++)
 	{
-		if (strcmp(args[i], "|") == 0) 
+		if (strcmp(args[i], "|") == 0)
 			num_cmds++;
 	}
 	char*** cmds = malloc((num_cmds + 1) * sizeof(char**)); // should output: {{"ls", "-lh", NULL}, {"cd", NULL}};
 
 	int cmd_index = 0;
 	int start = 0;
-	for (int i = 0; ;i++)
+	for (int i = 0;; i++)
 	{
-		if (args[i] == NULL || strcmp(args[i], "|") == 0) 
+		if (args[i] == NULL || strcmp(args[i], "|") == 0)
 		{
 			int argc = i - start;
 			cmds[cmd_index] = malloc((argc + 1) * sizeof(char*));
@@ -54,32 +56,34 @@ char*** split_pipes(char** args) // input: {"ls", "-lh", "|", "cd", NULL}
 				cmds[cmd_index][j] = args[start + j];
 			}
 			cmds[cmd_index][argc] = NULL;
-            cmd_index++;
-            if (args[i] == NULL) break;
-            start = i + 1;
+			cmd_index++;
+			if (args[i] == NULL)
+				break;
+			start = i + 1;
 		}
 	}
 	cmds[cmd_index] = NULL;
-    return cmds;
+	return cmds;
 }
 
-char** get_args(char** tokens) 
+char** get_args(char** tokens)
 {
-	int count = 0; 
-	
-	while(tokens[count] != NULL && count < TOK_BUFF_SIZE)
+	int count = 0;
+
+	while (tokens[count] != NULL && count < TOK_BUFF_SIZE)
 	{
 		count++;
 	}
-	
-	char** args = malloc((count + 1)* sizeof(char*));
-	if (!args) return NULL;
-	
+
+	char** args = malloc((count + 1) * sizeof(char*));
+	if (!args)
+		return NULL;
+
 	for (int i = 0; i < count; i++)
 	{
 		args[i] = tokens[i];
 	}
-	
+
 	args[count] = NULL;
 	return args;
 }
@@ -88,26 +92,26 @@ char** tokenize(char* line)
 {
 	int pos = 0;
 	int buff_size = TOK_BUFF_SIZE;
-	char** tokens = malloc(buff_size* sizeof(char*));
+	char** tokens = malloc(buff_size * sizeof(char*));
 	char* token;
-	
-	if (!tokens) 
+
+	if (!tokens)
 	{
 		fprintf(stderr, "allocation error\n");
 		exit(1);
 	}
-	
+
 	token = strtok(line, TOK_DELIM);
-	while(token != NULL)
+	while (token != NULL)
 	{
 		tokens[pos] = token;
 		pos++;
-		
+
 		if (pos >= buff_size)
 		{
 			buff_size += TOK_BUFF_SIZE;
-			tokens = realloc(tokens, buff_size* sizeof(char*));
-			if (!tokens) 
+			tokens = realloc(tokens, buff_size * sizeof(char*));
+			if (!tokens)
 			{
 				fprintf(stderr, "allocation error\n");
 				exit(1);
@@ -125,9 +129,10 @@ int sh_cd(char** args)
 	{
 		fprintf(stderr, "Expected argument: cd <directory>\n");
 	}
-	else 
+	else
 	{
-		if (chdir(args[1]) != 0) perror("cd");
+		if (chdir(args[1]) != 0)
+			perror("cd");
 	}
 	return 1;
 }
@@ -151,7 +156,6 @@ void run_single(char** args)
 	}
 
 	waitpid(c_pid, &status, 0);
-
 }
 
 void run_pipe(char*** commands)
@@ -166,87 +170,88 @@ void run_pipe(char*** commands)
 	}
 
 	p1 = fork();
-	if (p1 < 0) 
+	if (p1 < 0)
 	{
 		perror("Error");
 		exit(1);
 	}
-    if (p1 == 0)
-    {
-        close(pipefd[0]);
-        if (dup2(pipefd[1], STDOUT_FILENO) < 0) 
-		{ 
-			perror("dup2"); 
-			exit(1); 
+	if (p1 == 0)
+	{
+		close(pipefd[0]);
+		if (dup2(pipefd[1], STDOUT_FILENO) < 0)
+		{
+			perror("dup2");
+			exit(1);
 		}
-        close(pipefd[1]);
+		close(pipefd[1]);
 
-        execvp(commands[0][0], commands[0]);
-        perror("execvp");
-        exit(1);
-    }
-	    p2 = fork();
-    if (p2 < 0)
-    {
-        perror("fork");
-        return;
-    }
+		execvp(commands[0][0], commands[0]);
+		perror("execvp");
+		exit(1);
+	}
+	p2 = fork();
+	if (p2 < 0)
+	{
+		perror("fork");
+		return;
+	}
 
-    if (p2 == 0)
-    {
-        close(pipefd[1]);
-        if (dup2(pipefd[0], STDIN_FILENO) < 0) 
-		{ 
-			perror("dup2"); 
-			exit(1); 
+	if (p2 == 0)
+	{
+		close(pipefd[1]);
+		if (dup2(pipefd[0], STDIN_FILENO) < 0)
+		{
+			perror("dup2");
+			exit(1);
 		}
-        close(pipefd[0]);
+		close(pipefd[0]);
 
-        execvp(commands[1][0], commands[1]);
-        perror("execvp");
-        _exit(1);
-    }
+		execvp(commands[1][0], commands[1]);
+		perror("execvp");
+		_exit(1);
+	}
 
-    close(pipefd[0]);
-    close(pipefd[1]);
+	close(pipefd[0]);
+	close(pipefd[1]);
 
-    waitpid(p1, NULL, 0);
-    waitpid(p2, NULL, 0);
+	waitpid(p1, NULL, 0);
+	waitpid(p2, NULL, 0);
 }
 
 int main(void)
 {
 	printf("Mini shell in C!\n");
-	
+
 	char line[BUFF_SIZE];
 	char** tokens;
 	char** args;
 	char*** commands;
-	while(1) 
+	while (1)
 	{
 		fflush(stdout);
-		
+
 		printf(">>> ");
 		fgets(line, BUFF_SIZE, stdin);
-		
+
 		trim(line);
 		tokens = tokenize(line);
 		args = get_args(tokens);
 
-		if (args[0] == NULL) continue;
-		
+		if (args[0] == NULL)
+			continue;
+
 		if (strcmp(args[0], "cd") == 0)
 		{
 			sh_cd(args);
 			free(tokens);
 			continue;
 		}
-		if (strcmp(args[0], "exit") == 0) 
+		if (strcmp(args[0], "exit") == 0)
 		{
 			free(tokens);
 			break;
 		}
-		
+
 		if (check_pipes(args))
 		{
 			commands = split_pipes(args);
@@ -258,6 +263,6 @@ int main(void)
 			run_single(args);
 			free(tokens);
 		}
-	}	
+	}
 	return 0;
 }
